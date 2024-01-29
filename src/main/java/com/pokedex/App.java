ec73799f5a12f0f5d712828d548d2ed67042e01a
alexisc <a.chentreu@protonmail.com>
2024-01-29 11:51:07 +0100

    Get sprites URL + .png

--- src/main/java/com/pokedex/App.java
+++ src/main/java/com/pokedex/App.java
@@ -5,16 +5,22 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 //TODO 1 Faire des tests
 public class App {
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws IOException, ParseException, URISyntaxException {
         JSONParser jsonParser = new JSONParser();
         FileReader reader = new FileReader("src/main/jsonFiles/pokemonForm.json");
         Object obj = jsonParser.parse(reader);
         JSONArray pokemonArray = (JSONArray) obj;
-        int targetPokedexId = 1;
+        int targetPokedexId = 3;
         for (Object pokemonObj : pokemonArray) {
             JSONObject pokemon = (JSONObject) pokemonObj;
             long pokedexId = (long) pokemon.get("pokedexId");
@@ -22,7 +28,15 @@ public class App {
             if (pokedexId == targetPokedexId) {
                 JSONObject nameObject = (JSONObject) pokemon.get("name");
                 String frenchName = (String) nameObject.get("fr");
-                System.out.println("French name of first pokemon: " + frenchName);
+                System.out.println("French name of pokemon: " + frenchName);
+                JSONObject spriteObject = (JSONObject) pokemon.get("sprites");
+                String spriteUrlString = (String) spriteObject.get("regular");
+                URI spriteUrl = new URI(spriteUrlString);
+                BufferedImage spriteImage = ImageIO.read(spriteUrl.toURL());
+                String safeFrenchName = frenchName.replaceAll("[^a-zA-Z0-9.-]", "_");
+                ImageIO.write(spriteImage, "png", new File("src/main/pokemonSprites/" + safeFrenchName + ".png"));
+                System.out.println("Sprite URL of pokemon: " + spriteUrl);
+                System.out.println("Sprite image of pokemon: " + spriteImage);
                 break;
             }
         }
--- src/main/java/com/pokedex/HelloWorld.java
+++
@@ -1,4 +0,0 @@
-package com.pokedex;
-
-public class HelloWorld {
-}
